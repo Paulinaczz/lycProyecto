b45625722c382a2f92b2178dd29f8e2eecbcf9d8
cibel312 <cristianrodriguezhuaraz@gmail.com>
2025-03-01 02:58:04 -0500

    Traducción de reader.py a español

--- reader.py
+++ reader.py
@@ -3,97 +3,96 @@ from tokens import Token, TokenType
 LETRAS = 'abcdefghijklmnopqrstuvwxyz01234567890.'
 
 
-class Reader:
-    def __init__(self, string: str):
-        self.string = iter(string.replace(' ', ''))
-        self.input = set()
+class Lector:
+    def __init__(self, cadena: str):
+        self.cadena = iter(cadena.replace(' ', ''))
+        self.entrada = set()
         self.Siguiente()
 
     def Siguiente(self):
         try:
-            self.caracter_act = next(self.string)
+            self.caracter_actual = next(self.cadena)
         except StopIteration:
-            self.caracter_act = None
+            self.caracter_actual = None
 
     def CrearTokens(self):
-        while self.caracter_act != None:
+        while self.caracter_actual is not None:
 
-            if self.caracter_act in LETRAS:
-                self.input.add(self.caracter_act)
+            if self.caracter_actual in LETRAS:
+                self.entrada.add(self.caracter_actual)
                 yield Token(TokenType.LPAR, '(')
-                yield Token(TokenType.LETTER, self.caracter_act)
-
+                yield Token(TokenType.LETRA, self.caracter_actual)
+                
                 self.Siguiente()
-                parentesis_añadidos = False
+                parentesis_agregados = False
 
-                while self.caracter_act != None and \
-                        (self.caracter_act in LETRAS or self.caracter_act in '*+?'):
+                while self.caracter_actual is not None and \
+                        (self.caracter_actual in LETRAS or self.caracter_actual in '*+?'):
 
-                    if self.caracter_act == '*':
+                    if self.caracter_actual == '*':
                         yield Token(TokenType.KLEENE, '*')
                         yield Token(TokenType.RPAR, ')')
-                        parentesis_añadidos = True
+                        parentesis_agregados = True
 
-                    elif self.caracter_act == '+':
-                        yield Token(TokenType.PLUS, '+')
+                    elif self.caracter_actual == '+':
+                        yield Token(TokenType.MAS, '+')
                         yield Token(TokenType.RPAR, ')')
-                        parentesis_añadidos = True
+                        parentesis_agregados = True
 
-                    elif self.caracter_act == '?':
-                        yield Token(TokenType.QUESTION, '?')
+                    elif self.caracter_actual == '?':
+                        yield Token(TokenType.INTERROGACION, '?')
                         yield Token(TokenType.RPAR, ')')
-                        parentesis_añadidos = True
+                        parentesis_agregados = True
 
-                    elif self.caracter_act in LETRAS:
-                        self.input.add(self.caracter_act)
-                        yield Token(TokenType.APPEND)
-                        yield Token(TokenType.LETTER, self.caracter_act)
+                    elif self.caracter_actual in LETRAS:
+                        self.entrada.add(self.caracter_actual)
+                        yield Token(TokenType.CONCATENAR)
+                        yield Token(TokenType.LETRA, self.caracter_actual)
 
                     self.Siguiente()
 
-                    if self.caracter_act != None and self.caracter_act == '(' and parentesis_añadidos:
-                        yield Token(TokenType.APPEND)
+                    if self.caracter_actual is not None and self.caracter_actual == '(' and parentesis_agregados:
+                        yield Token(TokenType.CONCATENAR)
 
-                if self.caracter_act != None and self.caracter_act == '(' and not parentesis_añadidos:
+                if self.caracter_actual is not None and self.caracter_actual == '(' and not parentesis_agregados:
                     yield Token(TokenType.RPAR, ')')
-                    yield Token(TokenType.APPEND)
+                    yield Token(TokenType.CONCATENAR)
 
-                elif not parentesis_añadidos:
+                elif not parentesis_agregados:
                     yield Token(TokenType.RPAR, ')')
 
-            elif self.caracter_act == '|':
+            elif self.caracter_actual == '|':
                 self.Siguiente()
                 yield Token(TokenType.OR, '|')
 
-            elif self.caracter_act == '(':
+            elif self.caracter_actual == '(':
                 self.Siguiente()
                 yield Token(TokenType.LPAR)
 
-            elif self.caracter_act in (')*+?'):
+            elif self.caracter_actual in (')*+?'):
 
-                if self.caracter_act == ')':
+                if self.caracter_actual == ')':
                     self.Siguiente()
                     yield Token(TokenType.RPAR)
 
-                elif self.caracter_act == '*':
+                elif self.caracter_actual == '*':
                     self.Siguiente()
                     yield Token(TokenType.KLEENE)
 
-                elif self.caracter_act == '+':
+                elif self.caracter_actual == '+':
                     self.Siguiente()
-                    yield Token(TokenType.PLUS)
+                    yield Token(TokenType.MAS)
 
-                elif self.caracter_act == '?':
+                elif self.caracter_actual == '?':
                     self.Siguiente()
-                    yield Token(TokenType.QUESTION)
+                    yield Token(TokenType.INTERROGACION)
 
-                # Finally, check if we need to add an append token
-                if self.caracter_act != None and \
-                        (self.caracter_act in LETRAS or self.caracter_act == '('):
-                    yield Token(TokenType.APPEND, '.')
+                if self.caracter_actual is not None and \
+                        (self.caracter_actual in LETRAS or self.caracter_actual == '('):
+                    yield Token(TokenType.CONCATENAR, '.')
 
             else:
-                raise Exception(f'Invalid entry: {self.caracter_act}')
+                raise Exception(f'Entrada inválida: {self.caracter_actual}')
 
-    def GetSimbolos(self):
-        return self.input
+    def ObtenerSimbolos(self):
+        return self.entrada
